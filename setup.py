e8928cd8cdba09e23fca74c7a26868de2c38956c
alex.xi <alex.x.xi@juwai.com>
2020-02-13 11:27:46 +0800

    update setup.py remove pip related

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
-from pip._internal.download import PipSession
-from pip._internal.req import parse_requirements
-from setuptools import setup
 from setuptools import find_packages
+from setuptools import setup
 
 version = '1.0.0'
 
-install_reqs = parse_requirements('./requirements.txt', session=PipSession())
-requires = [str(ir.req) for ir in install_reqs]
+with open('requirements.txt') as f:
+    requires = f.read().strip().split('\n')
+
 
 setup(
     name="xmapper",
